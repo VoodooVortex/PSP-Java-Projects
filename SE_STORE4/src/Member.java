9f7053df5b8b17d39fac9cb2bda2d9b14b803e87
Pakkapon <Pakkapon2547@gmail.com>
2024-10-19 18:41:37 +0700

    add Iteration 4 (SE_STORE4)

---
+++ SE_STORE4/src/Member.java
@@ -0,0 +1,65 @@
+public class Member {
+    String id;
+    String firstName;
+    String lastName;
+    String email;
+    String passCode;
+    String phoneNum;
+    String point;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassCode() {
+        return passCode;
+    }
+
+    public void setPassCode(String passCode) {
+        this.passCode = passCode;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+
+    public void setPoint(String point) {
+        this.point = point;
+    }
+}
