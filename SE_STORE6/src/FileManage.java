108710c8c2cd95506d783631910bf1b70b01408a
Pakkapon <Pakkapon2547@gmail.com>
2024-10-19 18:43:08 +0700

    add Iteration 6 (SE_STORE6)

---
+++ SE_STORE6/src/FileManage.java
@@ -0,0 +1,278 @@
+import java.io.*;
+import java.util.*;
+
+public class FileManage {
+    Map<String, String> category = new HashMap<>();
+    List<Product> productList = new ArrayList<>();
+    List<Member> membersList = new ArrayList<>();
+
+    public void writeFile(String newLine, File file) throws IOException {
+        BufferedWriter writeFile = new BufferedWriter(new FileWriter(file, true));
+        writeFile.write(newLine);
+        writeFile.close();
+    }
+
+    //Method แสดงรายการสินค้าทั้งหมด | รับไฟล์
+    public void showCategory(File file) throws IOException {
+        BufferedReader readFile = new BufferedReader(new FileReader(file));
+
+        //ตัวแปรเอาไว้นับจำนวนรายการสินค้าทั้งหมด
+        int count = 1;
+
+        //อ่านไฟล์ category
+        String line;
+        while ((line = readFile.readLine()) != null){
+            String[] data = line.split("\\t");
+            category.put(String.valueOf(count), data[1].trim());
+            count++;
+        }
+
+        System.out.println("===== SE STORE's Product Categories =====");
+        System.out.printf("%-7s %-15s \n", "#", "Category");
+        for (String i : category.keySet()){
+            System.out.printf("%-7s %-15s \n", i, category.get(i));
+        }
+        System.out.println("=========================================");
+        readFile.close();
+    }
+
+    public void loadProduct(File file) throws FileNotFoundException {
+        Scanner readFile = new Scanner(file);
+        while (readFile.hasNext()) {
+            Product product = new Product();
+            product.setAmount(readFile.next().trim());
+            product.setName(readFile.next().trim());
+            product.setPrice(readFile.next().trim());
+            product.setQuantity(readFile.next().trim());
+            product.setId(readFile.next().trim());
+            productList.add(product);
+        }
+    }
+
+    //Method แสดงรายการสินค้าที่เราเลือก | รับไฟล์, รับตัวเลือกที่เลือก
+    public void showProductSelect(File file, String selectType) throws FileNotFoundException {
+        Scanner readFile = new Scanner(file);
+        //ตัวแปรไว้นับจำนวนรายการที่แสดง
+        int countProduct = 1;
+
+        //ลูปเอาไว้เช็คข้อมูลของ Product บรรทัดไหนบ้างที่ตรงกับ Category ที่เลือก
+        if (productList.isEmpty()) {
+            while (readFile.hasNext()) {
+                Product product = new Product();
+                product.setAmount(readFile.next().trim());
+                product.setName(readFile.next().trim());
+                product.setPrice(readFile.next().trim());
+                product.setQuantity(readFile.next().trim());
+                product.setId(readFile.next().trim());
+                productList.add(product);
+            }
+        }
+
+        String checkTypeId = "";
+        for (String i : category.keySet()){
+            checkTypeId = i.equals(selectType) ? String.valueOf(((Integer.parseInt(i)-1)+3000)) : "" ;
+            if (!checkTypeId.isEmpty()) break;
+        }
+
+        double discount = 0;
+        if (checkRole().equalsIgnoreCase("2")){
+            discount = 0.05;
+        }else if (checkRole().equalsIgnoreCase("3")){
+            discount = 0.10;
+        }
+
+
+        System.out.println("============ " + category.get(selectType) + " ============");
+        String title = discount != 0 ? String.format("%-5s %-17s %-12s %14s \n", "#" ,"Name", "Price (฿)", "Quantity")
+                : String.format("%-5s %-15s %-10s %6s \n", "#" ,"Name", "Price (฿)", "Quantity");
+        System.out.print(title);
+        for (Product i : productList){
+            if (i.getId().equals(checkTypeId)){
+                double price = Double.parseDouble(i.getPrice().substring(1)) * 34;
+                double disPrice = price - (price * discount);
+                String printPrice = String.format("%.2f", price);
+                String displayProduct = discount != 0 ? String.format("%-5d %-15s %-7.2f %-15s %s\n", countProduct, i.getName(), disPrice, "("+printPrice+")", i.getQuantity())
+                        : String.format("%-5s %-17s %-8.2f %5s \n", countProduct, i.getName(), price, i.getQuantity());
+                System.out.print(displayProduct);
+                countProduct++;
+            }
+        }
+
+        System.out.println("================================");
+    }
+
+    public void sortDESC(){
+        Product temp;
+        int min;
+        for (int i = 0; i < productList.size()-1; i++) {
+            min = i;
+            temp = productList.get(i);
+            for (int j = i+1; j < productList.size(); j++) {
+                if (productList.get(min).getName().compareTo(productList.get(j).getName()) < 0){
+                    min = j;
+                }
+            }
+            productList.set(i, productList.get(min));
+            productList.set(min, temp);
+        }
+    }
+
+    public void sortASC(){
+        Product temp;
+        int min;
+        for (int i = 0; i < productList.size()-1; i++) {
+            min = i;
+            temp = productList.get(i);
+            for (int j = i+1; j < productList.size(); j++) {
+                if (Integer.parseInt(productList.get(min).getQuantity()) > Integer.parseInt(productList.get(j).getQuantity())){
+                    min = j;
+                }
+            }
+            productList.set(i, productList.get(min));
+            productList.set(min, temp);
+        }
+    }
+
+    //Method เอาไว้นับจำนวนรายการสินค้า | รับไฟล์
+    public int getCountProduct() {
+        return category.size();
+    }
+
+    //Method เอาไว้เช็คว่า Email กับ Password ถูกมั้ย | รับไฟล์, email, password
+    public boolean checkCorrect(File file, String Email, String Password) throws FileNotFoundException {
+        Scanner readFile = new Scanner(file);
+        membersList.clear();
+
+        while (readFile.hasNext()){
+            Member member = new Member();
+
+            member.setId(readFile.next().trim());
+            member.setFirstName(readFile.next().trim());
+            member.setLastName(readFile.next().trim());
+            member.setEmail(readFile.next().trim());
+            member.setPassCode(readFile.next().trim());
+            member.setPhoneNum(readFile.next().trim());
+            member.setPoint(readFile.next().trim());
+
+            StringBuilder pass = new StringBuilder();
+            pass.append(member.getPassCode().charAt(9));//เพื่อค่า passCode ตำแหน่งที่ 9 .append ฟีลเหมือน pass += passCode.charAt(9)
+            pass.append(member.getPassCode().charAt(10));
+            pass.append(member.getPassCode().charAt(13));
+            pass.append(member.getPassCode().charAt(14));
+            pass.append(member.getPassCode().charAt(15));
+            pass.append(member.getPassCode().charAt(16));
+
+            if (Password.equals(String.valueOf(pass)) && Email.equals(member.getEmail())){//เช็คว่า email กับ password ตรงกับ ข้อมูลในไฟล์ มั้ย
+                membersList.add(member); //เก็บ email ที่ login ผ่านไว้
+                readFile.reset();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Method เอาไว้เช็คว่า Email กับ Password ถูกมั้ย
+    public boolean checkExpired() {
+        //ตัวแปรเอาไว้เก็บว่า บัญชีหมดอายุมั้ย ถ้าหมดเป็น false ถ้ายังไม่หมดเป็น true
+        boolean check = false;
+
+        for (Member i : membersList) {
+            check = i.passCode.charAt(2) == '1';
+        }
+
+        return check;
+    }
+
+    //Method เอาไว้แสดงข้อมูล member
+    public void showDataMember() {
+        //ตัวแปรเอาไว้เก็บข้อมูลที่ดัดแปลงแล้ว เพื่อเอาไปแสดง | **สังเกตุความแตกต่างที่พิมพ์ใหญ่ตัวแรก**
+        StringBuilder FirstName = new StringBuilder();
+        StringBuilder FullName = new StringBuilder("Hello, ");
+        StringBuilder Email = new StringBuilder();
+        StringBuilder Phone = new StringBuilder();
+        StringBuilder Point = new StringBuilder();
+
+        String role = "";
+
+        for (Member i : membersList){
+            //ชื่อ
+            FirstName.append(i.getLastName().charAt(0));//เอานามสกุลตัวแรก
+            FirstName.append(".");//เพิ่มจุด
+
+            if (i.getPassCode().charAt(6) == '0'){
+                role = "STAFF";
+            }if(i.getPassCode().charAt(6) == '1'){
+                role = "REGULAR";
+            }if (i.getPassCode().charAt(6) == '2'){
+                role = "SILVER";
+            }if (i.getPassCode().charAt(6) == '3'){
+                role = "GOLD";
+            }
+
+            FullName.append(FirstName).append(" ").append(i.getFirstName()).append(" (").append(role).append(")");//เอาชื่อมารวมกัน
+
+            //email
+            Email.append(i.getEmail());//เอา email ในไฟล์ไปใส่ในตัวแปรชนิด StringBuilder เพื่อง่ายต่อการดัดแปลง
+            Email.replace(2, i.getEmail().indexOf("@"), "***");//แทนที่ ตัวแหน่งที่ 2 ถึง ตำแหน่งที่ @ อยู่ ด้วย ***
+            Email.replace(Email.indexOf("@")+3, Email.length(), "***");//แทนที่ ตำแหน่งที่ @ อยู่ ถัดไป 3 ตัว ถึง ตำแหน่งสุดท้าย ด้วย ***
+
+            //เบอร์
+            Phone.append(i.getPhoneNum());
+            Phone.insert(3,"-");//แทรก (เพิ่ม) - ในตำแหน่งที่ 3
+            Phone.insert(7,"-");//แทรก (เพิ่ม) - ในตำแหน่งที่ 7
+
+            //point
+            Point.append(i.getPoint(), 0, i.getPoint().indexOf("."));//เอาตั้งแต่ตำแหน่งที่ 0 ไปจนถึงตำแหน่งก่อน ตำแหน่ง .
+        }
+
+        System.out.println("===== SE STORE =====");
+        System.out.println(FullName);
+        System.out.println("Email: " + Email);
+        System.out.println("Phone: " + Phone);
+        System.out.println("You have " + Point + " Point");
+
+    }
+
+    public String checkRole() {
+        String role = "";
+
+        for (Member i : membersList){
+            role = String.valueOf(i.getPassCode().charAt(6));
+        }
+
+        return role;
+    }
+
+    public String randomPassword(){
+        StringBuilder pass = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+            int randomInt = random.nextInt(10);
+            pass.append(randomInt);
+        }
+        return String.valueOf(pass);
+    }
+
+    public String randomChar(String pass){
+        String character = "ABCDEFGHIJKLMNOPQRSPUVWUSYZ";
+        StringBuilder passWord = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 19 ; i++) {
+            int randomInt = random.nextInt(26);
+            passWord.append(character.charAt(randomInt));
+        }
+
+        passWord.replace(2,3, "1");
+        passWord.replace(6,7,"1");
+        //passWord.setCharAt(9,pass.charAt(0));
+        passWord.replace(9,10,String.valueOf(pass.charAt(0)));
+        passWord.replace(10,11,String.valueOf(pass.charAt(1)));
+        passWord.replace(13,14,String.valueOf(pass.charAt(2)));
+        passWord.replace(14,15,String.valueOf(pass.charAt(3)));
+        passWord.replace(15,16,String.valueOf(pass.charAt(4)));
+        passWord.replace(16,17,String.valueOf(pass.charAt(5)));
+
+        return String.valueOf(passWord);
+    }
+}
---
+++ SE_STORE6/src/Main.java
@@ -0,0 +1,275 @@
+/************************************************************************************/
+/* Program Assignment: SE STORE */
+/* Student ID: 66160080 */
+/* Student Name: Pakkapon Chomchoey */
+/* Date: 14/08/67 */
+/* Description: โปรแกรมจำลองร้านค้า */
+
+import java.io.*;
+import java.util.Scanner;
+
+/*************************************************************************************/
+
+public class Main {
+
+    //หน้าแรกสุด หน้า Login
+    public static void displayLogin(){
+        System.out.println("===== SE STORE =====");
+        System.out.println("1. Login");
+        System.out.println("2. Exit");
+        System.out.println("====================");
+        System.out.print("Select (1-2) : ");
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner kb = new Scanner(System.in);
+        //ตัวแปรเก็บ Path File ทั้งหมด
+        File productData = new File("/Users/pk/Desktop/Year 2/Term 1/PSP/SE_STORE6/src/Product data.txt");
+        File categoryData = new File("/Users/pk/Desktop/Year 2/Term 1/PSP/SE_STORE6/src/Category data.txt");
+        File memberData = new File("/Users/pk/Desktop/Year 2/Term 1/PSP/SE_STORE6/src/Member data.txt");
+
+        //ตัวแปรเอาไว้เก็บตัวเลือก Login
+        String inputLogin;
+
+        //ตัวแปรเอาไว้เก็บตัวเลือกรายการสินค้า
+        String inputCategory;
+
+        FileManage fileManage = new FileManage();
+
+        do {
+            //ตัวแปรเอาไว้เก็บตัวเลือก Show Category หรือ Logout
+            String inputSelect = "";
+            String inputSelectStaff = "";
+
+            //รับตัวเลือก
+            displayLogin();
+            inputLogin = kb.nextLine();
+
+            switch (inputLogin){
+                case "1":
+                    //ตัวแปรเอาไว้เก็บจำนวนครั้งที่ใส่รหัสผิด
+                    int countError = 0;
+
+                    do {
+                        //รับ Email กับ Password
+                        System.out.println("===== LOGIN =====");
+                        System.out.print("Email : ");
+                        String email = kb.nextLine();
+                        System.out.print("Password : ");
+                        String password = kb.nextLine();
+
+                        //เช็คว่า Email กับ Password ถูกมั้ย
+                        if (fileManage.checkCorrect(memberData, email, password)){ //ถ้าถูก
+
+                            //เช็คว่า Expired (บัญชีหมดอายุ) มั้ย
+                            if (fileManage.checkExpired()){ //ถ้าถูก
+
+                                if (fileManage.checkRole().equals("0")){
+                                    fileManage.showDataMember();
+                                    do {
+                                        System.out.println("====================");
+                                        System.out.println("1. Show Category");
+                                        System.out.println("2. Add Member");
+                                        System.out.println("3. Logout");
+                                        System.out.println("====================");
+                                        System.out.print("Select (1-3) : ");
+                                        inputSelectStaff = kb.nextLine();
+                                        switch (inputSelectStaff) {
+                                            case "1":
+                                                do {
+                                                    //เรียกใช้ Method แสดงข้อมูล รายการสินค้า
+                                                    fileManage.showCategory(categoryData);
+
+                                                    //ตัวแปรเอาไว้เก็บจำนวน รายการสินค้า
+                                                    int count = fileManage.getCountProduct();
+
+                                                    System.out.println("Select Category to Show Product (1-" + count + ") or Q for exit");
+                                                    System.out.print("Select : ");
+                                                    inputSelect = kb.nextLine();
+
+                                                    switch (inputSelect) {
+                                                        case "1", "2", "3", "4", "5", "6", "7", "8", "9", "10":
+                                                            //เรียกใช้ Method แสดงข้อมูล รายการสินค้าที่เราเลือก
+                                                            fileManage.showProductSelect(productData, inputSelect);
+                                                            do {
+                                                                fileManage.productList.clear();
+                                                                System.out.println("1. Show Name By DESC");
+                                                                System.out.println("2. Show Quantity By ASC");
+                                                                System.out.print("or Press Q to Exit : ");
+                                                                inputCategory = kb.nextLine();
+                                                                switch (inputCategory){
+                                                                    case "1":
+                                                                            fileManage.loadProduct(productData);
+                                                                            fileManage.sortDESC();
+                                                                            fileManage.showProductSelect(productData, inputSelect);
+                                                                            fileManage.productList.clear();
+                                                                        break;
+                                                                    case "2":
+                                                                            fileManage.loadProduct(productData);
+                                                                            fileManage.sortASC();
+                                                                            fileManage.showProductSelect(productData, inputSelect);
+                                                                            fileManage.productList.clear();
+                                                                        break;
+                                                                }
+
+                                                            } while (!inputCategory.equalsIgnoreCase("Q"));//ถ้าที่เลือกมาไม่ใช่ q ให้วนลูปต่อ
+                                                            break;
+                                                    }
+
+                                                } while (!inputSelect.equalsIgnoreCase("Q"));//ถ้าที่เลือกมาไม่ใช่ q ให้วนลูปต่อ
+                                                break;
+
+                                            case "2":
+                                                addMember(kb,memberData);
+                                                break;
+
+                                            case "3":
+                                                System.out.println("===== SE STORE =====");
+                                                System.out.println("Thank you for using our service :3");
+                                                break;
+                                        }
+                                    }while (!inputSelectStaff.equals("3"));
+
+                                }else {
+                                    fileManage.showDataMember();
+                                    do {//ลูปแสดงตัวเลือกสินค้า
+                                        System.out.println("====================");
+                                        System.out.println("1. Show Category");
+                                        System.out.println("2. Logout");
+                                        System.out.println("====================");
+                                        System.out.print("Select (1-2) : ");
+                                        inputSelect = kb.nextLine();
+                                        switch (inputSelect){
+                                            case "1":
+                                                do {
+                                                    //เรียกใช้ Method แสดงข้อมูล รายการสินค้า
+                                                    fileManage.showCategory(categoryData);
+
+                                                    //ตัวแปรเอาไว้เก็บจำนวน รายการสินค้า
+                                                    int count = fileManage.getCountProduct();
+
+                                                    System.out.println("Select Category to Show Product (1-" + count + ") or Q for exit");
+                                                    System.out.print("Select : ");
+                                                    inputSelect = kb.nextLine();
+
+                                                    switch (inputSelect) {
+                                                        case "1", "2", "3", "4", "5", "6", "7", "8", "9", "10":
+                                                            //เรียกใช้ Method แสดงข้อมูล รายการสินค้าที่เราเลือก
+                                                            fileManage.showProductSelect(productData, inputSelect);
+                                                            do {
+                                                                fileManage.productList.clear();
+                                                                System.out.println("1. Show Name By DESC");
+                                                                System.out.println("2. Show Quantity By ASC");
+                                                                System.out.print("or Press Q to Exit : ");
+                                                                inputCategory = kb.nextLine();
+                                                                switch (inputCategory){
+                                                                    case "1":
+                                                                        fileManage.loadProduct(productData);
+                                                                        fileManage.sortDESC();
+                                                                        fileManage.showProductSelect(productData, inputSelect);
+                                                                        fileManage.productList.clear();
+                                                                        break;
+                                                                    case "2":
+                                                                        fileManage.loadProduct(productData);
+                                                                        fileManage.sortASC();
+                                                                        fileManage.showProductSelect(productData, inputSelect);
+                                                                        fileManage.productList.clear();
+                                                                        break;
+                                                                }
+                                                            } while (!inputCategory.equalsIgnoreCase("Q"));//ถ้าที่เลือกมาไม่ใช่ q ให้วนลูปต่อ
+                                                            break;
+                                                    }
+
+                                                }while (!inputSelect.equalsIgnoreCase("Q"));//ถ้าที่เลือกมาไม่ใช่ q ให้วนลูปต่อ
+                                                break;
+
+                                            case "2":
+                                                System.out.println("===== SE STORE =====");
+                                                System.out.println("Thank you for using our service :3");
+                                                break;
+                                        }
+
+                                    }while (!inputSelect.equals("2"));
+                                }
+                            }else {//ถ้า Expired (บัญชีหมดอายุ)
+                                System.out.println("===== LOGIN =====");
+                                System.out.println("Email : " + email);
+                                System.out.println("Password : " + password);
+                                System.out.println("====================");
+                                System.out.println("Error! - Your Account are Expired!");
+                                break;
+                            }
+
+                        }else {//ถ้าใส่ Email หรือ Password ผิด
+                            countError++;
+                            System.out.println("===== LOGIN =====");
+                            System.out.println("Email : " + email);
+                            System.out.println("Password : " + password);
+                            System.out.println("====================");
+                            System.out.println("Error! - Email or Password is Incorrect (" + countError + ")");
+                        }
+
+                    }while (countError != 3 && !inputSelect.equals("2") && !inputSelectStaff.equals("3"));//ถ้าใส่รหัสผิดไม่ถึง 3 ครั้ง และที่รับค่าเข้ามาไม่ใช่ 2 ให้วนลูปต่อ
+
+                    //ถ้าใส่รหัสผิด 3 ครั้ง
+                    if (countError == 3){
+                        System.out.println("Sorry, Please try again later :(");
+                    }
+                    break;
+
+                case "2":
+                    System.out.println("===== SE STORE =====");
+                    System.out.println("Thank you for using our service :3");
+                    break;
+
+            }
+
+        }while (!inputLogin.equals("2"));//ถ้าที่รับค่าเข้ามาไม่ใช่ 2 ให้วนลูปต่อ
+
+    }
+
+    public static void addMember(Scanner kb, File memberData) throws IOException {
+        FileManage fileManage = new FileManage();
+        System.out.println("====Add Member====");
+        System.out.print("Firstname : ");
+        String firstName = kb.nextLine();
+        System.out.print("Lastname : ");
+        String lastName = kb.nextLine();
+        System.out.print("Email : ");
+        String email = kb.nextLine();
+        System.out.print("Phone : ");
+        String phone = kb.nextLine();
+
+        if (firstName.length() > 2 && lastName.length() > 2 && email.contains("@") && phone.length() == 10){
+            BufferedReader read = new BufferedReader(new FileReader(memberData));
+            String line;
+            String ID = "";
+            while ((line = read.readLine()) != null){
+                String[] data = line.split("\\s+");
+                ID = data[0].trim();
+            }
+
+            String newID = String.valueOf(Integer.parseInt(ID)+1);
+
+            StringBuilder inputFileWriter = new StringBuilder();
+            String passNum = fileManage.randomPassword();
+
+            inputFileWriter.append(newID).append("\t")
+                    .append(firstName).append("\t")
+                    .append(lastName).append("\t")
+                    .append(email).append("\t")
+                    .append(fileManage.randomChar(passNum)).append(" ")
+                    .append(phone).append("  ").append("0.00");
+
+            System.out.println("Success - New Member has been created!");
+            System.out.println(firstName + "'" + lastName.substring(0, 1).toLowerCase() + " Password is " + passNum);
+
+            fileManage.writeFile("\n" + inputFileWriter, memberData);
+
+        }else{
+            System.out.println("Error! = - Your Information are Incorrect!");
+        }
+    }
+
+}
